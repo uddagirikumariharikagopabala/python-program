934001a036318ce4e2368b5e5a689b4d43e05c1f
uddagirikumariharikagopabala <uddagirikumariharikagopabala@gmail.com>
2026-09-10 20:13:09 +0530

    inheritance with attribute

---
+++ inheritance with attribute.py
@@ -0,0 +1,15 @@
+class college:
+    name="KPRIT"
+class student(college):
+    branch="AIML"
+s1=student()
+print(s1.name)
+print(s1.branch)
+    
+        
+        
+    
+
+
+                
+        
