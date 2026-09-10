b6bfeaff4a8f082716e0835a5459879545e17e5d
uddagirikumariharikagopabala <uddagirikumariharikagopabala@gmail.com>
2026-09-10 20:22:12 +0530

    Method overiding

---
+++ method overriding.py
@@ -0,0 +1,17 @@
+class animal:
+    def walk(self):
+        print("Animal can walk")
+class Lion(animal):
+    def walk(self):
+        print("Lion is the king of jungle")
+l1=Lion()
+l1.walk()
+
+    
+        
+        
+    
+
+
+                
+        
