d32a1b11244fe9c936497b778bf5a6e1cab0c649
uddagirikumariharikagopabala <uddagirikumariharikagopabala@gmail.com>
2026-09-10 21:09:48 +0530

    Hierarchical inheritance

---
+++ Hierarchical inheritance.py
@@ -0,0 +1,25 @@
+class branch:
+    def courses(parent):
+        print("CSE course")
+class child1:
+    def study(self):
+        print("AIML course")
+class child2:
+    def study(self):
+        print("DS course")
+c1=child1()
+d1=child2()
+p=branch()
+p.courses()
+c1.study()
+d1.study
+
+
+    
+        
+        
+    
+
+
+                
+        
