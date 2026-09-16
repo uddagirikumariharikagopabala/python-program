7ebe4630bd48a8c6f630faec2d821efe6c10510b
uddagirikumariharikagopabala <uddagirikumariharikagopabala@gmail.com>
2026-09-16 21:45:29 +0530

    Add files via upload
    Inheritance with methods

---
+++ Inheritance with methods.py
@@ -0,0 +1,19 @@
+class animal:
+    def eat(self):
+        print("Animal can eat")
+class dog(animal):
+    def speak(self):
+        print("dog bark")
+d1=dog()
+d1.eat()
+d1.speak()
+        
+
+    
+        
+        
+    
+
+
+                
+        
