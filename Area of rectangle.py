abc633a13af7af9b7ca231a3934e5974c4f94838
uddagirikumariharikagopabala <uddagirikumariharikagopabala@gmail.com>
2026-09-16 21:51:12 +0530

    Add files via upload
    Area of rectangle

---
+++ Area of rectangle.py
@@ -0,0 +1,20 @@
+class rectangle:
+    def area(self,l,b):
+        print(l*b)
+    def perimeter(self,l,b):
+        print(2*l+2*b)
+l=int(input("Enter l: "))
+b=int(input("Enter b: "))
+rec=rectangle()
+rec.area(l,b)
+rec.perimeter(l,b)
+        
+
+    
+        
+        
+    
+
+
+                
+        
