942069bb7ec95c75064719913af8f3a94d4d35db
uddagirikumariharikagopabala <uddagirikumariharikagopabala@gmail.com>
2026-09-16 21:42:10 +0530

    Add files via upload
    Inheritance with constructor

---
+++ Inheritance with constructor.py
@@ -0,0 +1,22 @@
+class Demo:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+class sample(Demo):
+    def info(self):
+        print(self.name)
+        print(self.age)
+s1=sample("abc",12)
+s1.info()
+s2=sample("cad",14)
+s2.info()
+        
+
+    
+        
+        
+    
+
+
+                
+        
