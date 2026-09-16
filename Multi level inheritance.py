b707ae29fc7929f8964bf00bb6a0468320d3e80c
uddagirikumariharikagopabala <uddagirikumariharikagopabala@gmail.com>
2026-09-16 21:36:39 +0530

    Add files via upload
    Multi level inheritance

---
+++ Multi level inheritance.py
@@ -0,0 +1,25 @@
+class animal:
+    def eat(self):
+        print("Animal can eat")
+class dog(animal):
+    def sleep(self):
+        print("Dog sleeps")
+class puppy(dog):
+    def speak(self):
+        print("puppy")
+p1=animal()
+p1.eat()
+p2=dog()
+p2.sleep()
+p3=puppy()
+p3.speak()
+        
+
+    
+        
+        
+    
+
+
+                
+        
