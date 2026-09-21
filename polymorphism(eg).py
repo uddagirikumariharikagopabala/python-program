16bc128aa00f5a617a64bac0bdc8aa8d496d8a09
uddagirikumariharikagopabala <uddagirikumariharikagopabala@gmail.com>
2026-09-21 15:33:02 +0530

    Add files via upload
    example of polymorphism

---
+++ polymorphism(eg).py
@@ -0,0 +1,31 @@
+class shape:
+    def area(self):
+        print("cone")
+class circle:
+    def area(self):
+        pie=3.14
+        r=9
+        print("Area of circle=",pie*r*r)
+class rectangle:
+    def area(self):
+        l=5
+        b=10
+        print("Area of rectangle=",l*b)
+class triangle:
+    def area(self):
+        b=5
+        h=5
+        print("Area of traiangle=",0.5*b*h)
+s=shape()
+s.area()
+c=circle()
+c.area()
+r=rectangle()
+r.area()
+t=triangle()
+t.area()
+
+
+        
+        
+        
